b2f6e07985b1165297bc1957663cbe3e1f959749
73begonia <1782015847@qq.com>
2024-05-09 17:24:43 +0800

    Hazel now builds as a static library + fixed warnings.

--- Hazel/vendor/GLAD/premake5.lua
+++ Hazel/vendor/GLAD/premake5.lua
@@ -1,6 +1,7 @@
 project "Glad"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -13,13 +14,17 @@ project "Glad"
     }
 
 	includedirs
-	{
-		"include"
-	}
+    {
+        "include"
+    }
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
